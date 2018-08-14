1969c75b07e3b0743d3650419e43984fedc794a4
Ilya Aleshin <>
2018-08-14 20:34:36 +0300

    init

---
+++ ExtensionAndUITableViewCell/InfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InfoViewController.swift
+//  ViewControllersAndLogs
+//
+//  Created by Ilya Aleshin on 21.06.2018.
+//  Copyright © 2018 Bakh. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("Info экран был показан")
+    }
+    
+}
